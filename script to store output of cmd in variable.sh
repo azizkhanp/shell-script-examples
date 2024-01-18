8a713e91a2fddc93df6e9d74c017a673407c737d
pathan <azizkhan.cool@gmail.com>
2024-01-18 15:49:19 +0530

    Update script to store output of cmd in variable.sh

--- script to store output of cmd in variable.sh
+++ script to store output of cmd in variable.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+# debug the script
+set -x 
+#run hostname command and store output in host_name variable
 host_name=$(hostname)
+# to check above command is executed correctly or not
+if [ $? - eq 0 ]; then
 echo "my server name is: $host_name"
+fi
