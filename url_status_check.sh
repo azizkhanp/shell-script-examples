dcb9d186c4d11e771a1243a74fb751ee96329b9c
pathan <azizkhan.cool@gmail.com>
2025-06-11 15:48:41 +0530

    Update url_status_check.sh

--- url_status_check.sh
+++ url_status_check.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
-url = "https://www.youtube.com/"
+url = "www.youtube.com"
 
 if ! curl -s --head --request GET "$url" | grep "200 OK" >/dev/null;then
 echo "$(date): $url is down" >> status.logs
 else
 echo "$(date): $url is up" >> status.logs
 fi
+
+---------------------------------------------------------------------------------
+
+#!/bin/bash
+
+url="www.google.com"
+
+# Repeat 3 times (i.e., check every 5 minutes for 15 minutes total)
+for i in {1..3}; do
+  if ! curl -s --head --request GET "$url" | grep "200 OK" > /dev/null; then
+    echo "$(date): $url is down" >> status.logs
+  else
+    echo "$(date): $url is up" >> status.logs
+  fi
+
+  # Wait for 5 minutes (300 seconds) before next check
+  sleep 300
+done
